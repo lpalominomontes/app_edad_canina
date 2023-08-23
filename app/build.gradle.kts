f91db5d22bd3abd7806d0a62a04e76bcf0259a46
lpalominoSMU <111005157+lpalominoSMU@users.noreply.github.com>
2023-08-23 01:24:55 -0400

    primer commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //id("kotlin-android-extensions")
+    id("kotlin-kapt")
 }
 
 android {
@@ -26,12 +28,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    dataBinding {
+        enable = true
     }
 }
 
--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //forma antigua de llamar a la vista
+        /*setContentView(R.layout.activity_main)*/
+
+        /*val ageEdit = binding.ageEdit
+        val resultText = binding.resultText
+        val button = binding.button*/
+
+        //forma antigua de llamar a los id
+        /*val ageEdit = findViewById<EditText>(R.id.age_edit)
+        val resultText = findViewById<TextView>(R.id.result_text)
+        val button = findViewById<Button>(R.id.button)*/
+
+        //crear interactiones en log cat
+        /*Log.v("MainActivity", "Activity created verbose")
+        Log.d("MainActivity", "Activity created debug")
+        Log.i("MainActivity", "Activity created info")
+        Log.w("MainActivity", "Activity created warning")
+        Log.e("MainActivity", "Activity created error")*/
+
+        binding.button.setOnClickListener {
+            val ageString = binding.ageEdit.text.toString()
+            if (ageString.isNotEmpty()) {
+                val ageInt = ageString.toInt()
+                val result = ageInt * 7
+                binding.resultText.text = getString(R.string.result_text, result)
+            } else {
+                Log.d("MainActivity", "Age string is empty")
+                Toast.makeText(this, R.string.you_must_insert_your_age, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
